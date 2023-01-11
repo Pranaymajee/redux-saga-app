dd332ba622d0b17e956efcc91fa64b0afe0f13d0
Pranay Majee <pranay.majee@gmail.com>
2023-01-11 14:07:22 +0530

    comment fix

--- src/redux/sagas/newsSaga.js
+++ src/redux/sagas/newsSaga.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 /* yield -> IT IS A BUILT-IN FUNCTION WHICH ALLOWS TO USE GENERATOR FUNCTIONS SEQUENTIALLY.
 WHEN USED IN JAVASCRIPT, THE GENERATOR FUNCTIONS WILL ALLOW TO YIELD ALL VALUES FROM THE NESTED FUNCTIONS. */
 
-/* call -> call is a blocking effect creator.
-This means that the saga will not continue to run to the next yield until the API call finishes. */
+/* call -> call IS A BLOCKING EFFECT CREATOR.
+THIS MEANS THAT THE SAGA WILL NOT CONTINUE TO RUN TO THE NEXT yield UNTIL THE API CALL FINISHES. */
 
 /* put -> ONCE call IS FINISHED, WE yield put.
 put IS DISPATCHING A NEW ACTION WITH THE RESULT FROM THE PREVIOUS YIELD i.e call */
